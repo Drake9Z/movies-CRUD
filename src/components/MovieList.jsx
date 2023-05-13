00eb725fc45c722c5b3ecdab398a9604c6da75ac
Juan Alfonso <juanchox197@gmail.com>
2023-05-12 21:44:54 -0500

    button close in list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const MovieList = ({initialMovies}) => {
-
-    console.log(initialMovies);
-    return (
-        <div className='container-list'>
-            <ul>
-                {initialMovies.map((movie) => (
-                    <li key={movie.id} className="movie-card">
-                        <h2>{movie.name}</h2>
-                        <p>Genero: {movie.genre}</p>
-                        <p>Duración: {movie.duration}</p>
-                        <p>Estreno: {movie.release_date}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+const MovieList = ({ initialMovies }) => {
+  console.log(initialMovies);
+  return (
+    <div className="container-list">
+      <ul>
+        {initialMovies.map((movie) => (
+          <li key={movie.id} className="movie-card">
+            <button
+              className="button-delete"
+              onClick={() => deleteMovie(movie.id)}
+            >
+              <i className="bx bx-x-circle"></i>
+            </button>
+            <h2>{movie.name}</h2>
+            <p>Genero: {movie.genre}</p>
+            <p>Duración: {movie.duration}</p>
+            <p>Estreno: {movie.release_date}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieList;
