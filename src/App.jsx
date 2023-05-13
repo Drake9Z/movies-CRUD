fd5331a88a13a4c764b2f7d8ef0255cc8f620222
Juan Alfonso <juanchox197@gmail.com>
2023-05-12 20:38:21 -0500

    movies CRUD

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import React, {useState} from 'react'
+import MovieList from "./components/MovieList"
+import Movieform from "./components/MovieForm"
+
+function App() {
+
+  return (
+    <div className='App'>
+      
+    </div>
+  )
+}
+
+export default App
