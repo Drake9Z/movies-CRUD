f55f31754dcf7cc971e21f647f77309746c4ab64
Juan Alfonso <juanchox197@gmail.com>
2023-05-12 22:05:36 -0500

    otro commit

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
 const MovieForm = () => {
+
+const [name, setName] = useState("")    
+const [genre, setGenre] = useState("")
+const [duration, setDution] = useState("")
+const [release_date, setRelease_date] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const newMovie = {
+            name,
+            genre,
+            duration,
+            release_date
+        }
+    }
+
   return (
     <div className="movieForm">
       <h2>Form</h2>
-      <form action="">
+      <form onSubmit={e => hadleSubmit(e)}>
         <label htmlFor="name">Nombre: </label>
         <input 
         id="name"
-        type="text" />
+        type="text"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        />
         <label htmlFor="genre">Género</label>
         <input 
         id="genre"
-        type="text" />
+        type="text" 
+        />
         <label htmlFor="duration">Duración</label>
         <input 
         id="duration"
-        type="text" />
+        type="text" 
+        value={name}
+        onChange={e => setName(e.target.value)}
+        />
         <label htmlFor="release_date">Estreno</label>
         <input 
         id="release_date"
-        type="text" />
+        type="text" 
+        value={name}
+        onChange={e => setName(e.target.value)}
+        />
       </form>
 
       <button type='submit'>Crear película</button>
